ab02920df61590a8f7c367f6194cdc407d20c68f
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:14:51 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs22.js
@@ -0,0 +1,5 @@
+function tugas22() {
+    var kata = ["saya", "ingin", "belajar", "bersama"];
+    kata.forEach((element , index) => console.log("item : "+element+" Index ke "+index));
+}
+tugas22();
